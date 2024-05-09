701d8ffb0a08bd0bd29b04c56a7e6d5a7fd74602
jkedra <jkedra@gmail.com>
2024-05-10 01:25:23 +0200

    Removed useless stuff from program.cs

--- MultigraphEditor/Program.cs
+++ MultigraphEditor/Program.cs
@@ -6,9 +6,6 @@ namespace MultigraphEditor
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
@@ -19,8 +16,6 @@ namespace MultigraphEditor
             mGraphAlgorithms.Add(typeof(DijkstraAlgorithm));
             mGraphAlgorithms.Add(typeof(CheckHamiltonCycle));
 
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new MainForm(NodeType, EdgeType, LayerType, mGraphAlgorithms));
         }
